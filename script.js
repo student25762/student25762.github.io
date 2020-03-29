65d7e3aa419f3cb9e0cba52981220edb78f935de
student25762 <hubertkrz@gmail.com>
2020-03-29 20:20:55 +0200

    task 21.6v2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://localhost:8080/v1/task/';
+  var apiRoot = 'https://young-caverns-66542.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
