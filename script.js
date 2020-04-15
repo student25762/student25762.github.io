fe67f9a04d9700ea1dd6902da96e82a2c63d62a3
student25762 <hubertkrz@gmail.com>
2020-04-15 12:35:36 +0200

    24.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://young-caverns-66542.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://young-caverns-66542.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://api.trello.com/1';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
